e151b76de52df0ab9bfd267f6759b53b3041ad37
Marius Sørensen <jobb@s0rensen.no>
2021-11-21 21:59:13 +0100

    Update requirements

--- image-client/image-client.py
+++ image-client/image-client.py
@@ -5,4 +5,4 @@ from torchvision.transforms import ToTensor
 from utils import *
 
 images = preprocess(MNIST('./data', download=True, transform=ToTensor()))
-show_images(images[0:5])
+print(images[0:5])
